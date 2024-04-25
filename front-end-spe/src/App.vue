9add57a4f01d06378ef8c6acb6fc8ccdb12401f9
abderrahmane-18 <abderrahmane.boukhezar@gmail.com>
2024-04-25 15:08:24 +0100

    fix login

--- front-end-spe/src/App.vue
+++ front-end-spe/src/App.vue
@@ -1,26 +1,25 @@
-
-<script setup >
-import Admin from './views/Admin.vue';
-import Tables from './views/Tables.vue';
-import NavBar from './views/NavBar.vue';
-import Nav from './components/Nav.vue';
-
-import Dashboard from './views/Dashboard.vue';
-
-
-</script>
-
 <template>
-  <main>
-    <Nav/>
-<RouterView/>
-
-  
-
-
-</main>
+  <div>
+    <router-view />
+  </div>
 </template>
 
-<style >
+<script setup>
+import { onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
+// Check if the user is authenticated on mount
+onMounted(() => {
+  const route = useRoute();
+  const router = useRouter();
+  
+  const isAuthenticated = localStorage.getItem('access_token');
+  if (!isAuthenticated && route.name !== 'Login') {
+    router.push({ name: 'Login' });
+  }
+});
+</script>
+
+<style>
+/* Your styling here */
 </style>
--- front-end-spe/src/router/index.js
+++ front-end-spe/src/router/index.js
@@ -1,25 +1,42 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Login from "../components/Login.vue";
-import HelloWorld from "../components/HelloWorld.vue";
-
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '../views/Login.vue';
+import Dashboard from '../views/Dashboard.vue';
 
 const routes = [
-  { path: "/", name: "HelloWorld", component: HelloWorld },
   {
-    path: '/login',
-    name: "Login",
-    component: ()=>import("../components/Login.vue"),
+    path: '/',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    meta: { requiresAuth: true },
   },
   {
     path: '/admin',
-    name: "Admin",
-    component: ()=>import("../views/Admin.vue"),
-  }
+    name: 'Admin',
+    component: () => import('../views/Admin.vue'),
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const isAuthenticated = localStorage.getItem('access_token');
+    if (!isAuthenticated) {
+      next('/');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
