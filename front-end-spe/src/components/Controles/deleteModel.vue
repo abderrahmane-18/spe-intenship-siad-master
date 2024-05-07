a7951a1b17bb722dcc9d15f9140a0e43926b89cf
abderrahmane-18 <abderrahmane.boukhezar@gmail.com>
2024-05-07 16:29:13 +0100

    delete

---
+++ front-end-spe/src/components/Controles/deleteModel.vue
@@ -0,0 +1,39 @@
+<template>
+    <div v-if="showModal" class="overflow-x-hidden overflow-y-auto fixed top-4 left-0 right-0 md:inset-0 z-50 justify-center items-center h-modal sm:h-full" style="position: absolute; left: 265px; top: 124px;">
+      <div class="relative w-full max-w-md px-4 h-full md:h-auto">
+        <!-- Modal content -->
+        <div class="bg-gray rounded-lg shadow relative">
+          <!-- Modal header -->
+          <div class="flex justify-end p-2">
+            <button type="button" @click="emit('close')" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-toggle="delete-product-modal">
+              <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
+            </button>
+          </div>
+          <!-- Modal body -->
+          <div class="p-6 pt-0 text-center">
+            <svg class="w-20 h-20 text-red-600 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <h3 class="text-xl font-normal text-gray-500 mt-5 mb-6">{{ modalTitle }}</h3>
+            <button @click="emit('delete', itemToDelete)" class="text-white bg-orange-600 hover:bg-orange-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2">
+              Yes, I'm sure
+            </button>
+            <button @click="emit('close')" class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center" data-modal-toggle="delete-product-modal">
+              No, cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps, defineEmits } from 'vue'
+  
+  const props = defineProps({
+    showModal: Boolean,
+    modalTitle: String,
+    itemToDelete: Object
+  })
+  
+  const emit = defineEmits(['close', 'delete'])
+  </script>
+  
